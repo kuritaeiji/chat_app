4cd6fca2bc7d28f130b6777a99191971f00cce94
kuritaeiji <eiji199825@yahoo.co.jp>
2020-07-02 18:38:23 +0900

    no message

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -2,6 +2,11 @@ import './plugins/bootstrap-vue.js'
 import './plugins/turbolinks'
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
